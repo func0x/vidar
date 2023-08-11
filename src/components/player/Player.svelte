e2acad2c012981a5d3962cfba42d8c4f1234dc9c
Blazej Witczak <bwitczak@svevn.com>
2023-08-11 10:09:05 +0200

    [#39953] Fix mobile overflow

--- src/components/player/Player.svelte
+++ src/components/player/Player.svelte
@@ -1,18 +1,39 @@
 <script>
+	import MediaQuery from 'src/hooks/UseMediaQuery.svelte';
 	import Box from 'src/components/Box.svelte';
 	import noVideo from '$lib/images/no_video.svg';
 </script>
 
-<Box height="720px" bg="black" cvh>
-	<Box df gap="var(--gap-xl)" width="fit-content" height="fit-content" bg="transparent" ch>
-		<img src={noVideo} alt="no-video" />
-		<span>VIDEO NOT YET AVAILABLE</span>
-	</Box>
-</Box>
+<MediaQuery query="(min-width: 1115px)" let:matches>
+	{#if matches}
+		<Box height="720px" bg="black" cvh>
+			<Box df gap="var(--gap-xl)" width="fit-content" height="fit-content" bg="transparent" ch>
+				<img src={noVideo} alt="no-video" />
+				<span>VIDEO NOT YET AVAILABLE</span>
+			</Box>
+		</Box>
+	{:else}
+		<Box height="720px" bg="black" cvh>
+			<Box
+				df
+				fd="column"
+				gap="var(--gap-xl)"
+				width="fit-content"
+				height="fit-content"
+				bg="transparent"
+				ch
+			>
+				<img src={noVideo} alt="no-video" />
+				<span>VIDEO NOT YET AVAILABLE</span>
+			</Box>
+		</Box>
+	{/if}
+</MediaQuery>
 
 <style>
 	span {
 		color: var(--white);
 		font-size: var(--font-xl);
+		text-align: center;
 	}
 </style>
