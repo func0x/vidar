fdde4a17d80131c1ec39b43f8e732a76dc013ec5
Blazej Witczak <bwitczak@svevn.com>
2023-08-11 10:12:14 +0200

    [#41679] Add external assets functionality

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,34 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import path from 'path';
 import json from '@rollup/plugin-json';
+import copy from 'rollup-plugin-copy';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit(), json({ compact: true })],
+	plugins: [
+		sveltekit(),
+		json({ compact: true }),
+		copy({
+			targets: [
+				{
+					src: './external_assets/images',
+					dest: './src/lib',
+					force: true
+				},
+				{
+					src: './external_assets/styles.css',
+					dest: './src/routes',
+					force: true
+				},
+				{
+					src: './external_assets/images/vidar.svg',
+					dest: './static/',
+					force: true
+				}
+			],
+			copyOnce: true
+		})
+	],
 	resolve: {
 		alias: {
 			src: path.resolve('src/')
