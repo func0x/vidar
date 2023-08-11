ce5ec520757bc542c8764c2303c09ea086e67b3e
Blazej Witczak <bwitczak@svevn.com>
2023-08-11 10:08:09 +0200

    [#38954] Create media query component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import 'src/routes/styles.css';
+	import DesktopNavigation from 'src/components/navigation/DesktopNavigation.svelte';
+	import MediaQuery from 'src/hooks/UseMediaQuery.svelte';
+</script>
+
+<MediaQuery query="(min-width: 1115px)" let:matches>
+	{#if matches}
+		<DesktopNavigation />
+	{/if}
+</MediaQuery>
+
+<slot />
