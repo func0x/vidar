08460a8b7f5f01b4df739a7fff91f2c1c33bc98e
Blazej Witczak <bwitczak@svevn.com>
2023-08-11 10:08:14 +0200

    [#38954] Add Box.svelte

---
+++ src/components/Box.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { css } from '@emotion/css';
+
+	export let cvh = false; // center vertical and horizontal
+	export let ch = false; // center horizontal
+	export let jsb = false; // justify-content: space-between
+	export let width = '100%';
+	export let height = '100%';
+	export let padding = 'var(--gap-l)';
+
+	let boxCss = css`
+		width: ${width};
+		height: ${height};
+		padding: 0 ${padding};
+	`;
+</script>
+
+<div class:cvh class:ch class:jsb class={boxCss}>
+	<slot />
+</div>
+
+<style>
+	.cvh {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.ch {
+		display: flex;
+		align-items: center;
+	}
+
+	.jsb {
+		justify-content: space-between;
+	}
+</style>
