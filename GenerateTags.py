f1d8731f44d3285bc0e0e8ce2c617ce2820610ce
Blazej Witczak <bwitczak@svevn.com>
2023-08-11 10:09:04 +0200

    [#39857] Fix GenerateTags.py to omit authors.json during generate tags

--- GenerateTags.py
+++ GenerateTags.py
@@ -4,15 +4,20 @@ from types import SimpleNamespace
 tags = open('./src/lib/jsons/tags.json', 'w')
 
 uniqueTags = set()
-for filename in glob.iglob('./src/lib/event_data/**/*.json', recursive=True):
+for filename in glob.iglob('./event_data/**/*.json', recursive=True):
     data = open(filename, 'r')
     jsonData = json.load(data)
     dumpData = json.dumps(jsonData)
 
     serializeData = json.loads(dumpData, object_hook=lambda d: SimpleNamespace(**d))
-    if serializeData.upcoming == False:
-        uniqueTags.update(serializeData.tags)
 
+    try:
+        serializeData.upcoming
+    except AttributeError:
+        pass
+    else:
+        if serializeData.upcoming == False:
+            uniqueTags.update(serializeData.tags)
 
     data.close()
 
