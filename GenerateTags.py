d9dab2512b49040bb57a9c5d39019067f7c46a5d
Blazej Witczak <bwitczak@svevn.com>
2023-08-17 09:26:09 +0200

    [#39857] Fix GenerateTags.py

--- GenerateTags.py
+++ GenerateTags.py
@@ -4,7 +4,7 @@ from types import SimpleNamespace
 tags = open('./src/lib/jsons/tags.json', 'w')
 
 uniqueTags = set()
-for filename in glob.iglob('./event_data/**/*.json', recursive=True):
+for filename in glob.iglob('./src/lib/event_data/**/*.json', recursive=True):
     data = open(filename, 'r')
     jsonData = json.load(data)
     dumpData = json.dumps(jsonData)
